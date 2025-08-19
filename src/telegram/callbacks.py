696500844dc626f26b022fa4548e52908caddef2
Drakovthe6th <drakov1@outlook.com>
2025-08-18 17:36:31 -0800

    startup

--- src/telegram/callbacks.py
+++ src/telegram/callbacks.py
@@ -8,7 +8,7 @@ from src.database.mongo import (
 )
 from src.features.otc_desk import otc_desk
 from src.features.quests import complete_quest
-from src.integrations.tonE2 import process_ton_withdrawal
+from src.integrations.ton import process_ton_withdrawal
 from src.utils.conversions import to_ton, convert_currency, calculate_fee, game_coins_to_ton
 from src.utils.validators import validate_ton_address, validate_mpesa_number, validate_email
 from config import Config
