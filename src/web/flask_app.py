d70e7b5898e7052161cbabf64b1ee509de4455ed
Drakovthe6th <drakov1@outlook.com>
2025-08-21 09:22:12 -0800

    new era 5th

--- src/web/flask_app.py
+++ src/web/flask_app.py
@@ -18,3 +18,7 @@ def create_app():
     configure_routes(app)
     
     return app
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
