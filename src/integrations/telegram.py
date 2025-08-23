7aa5560dc6df2b49ad21d012128ff1c28234a8af
Drakovthe6th <drakov1@outlook.com>
2025-08-23 11:57:04 -0800

    remodifications

--- src/integrations/telegram.py
+++ src/integrations/telegram.py
@@ -3,7 +3,7 @@ from telethon.sessions import StringSession
 import requests
 import logging
 from config import config
-from src.integrations.ton import validate_wallet
+from src.features.quests import validate_wallet
 
 logger = logging.getLogger(__name__)
 
